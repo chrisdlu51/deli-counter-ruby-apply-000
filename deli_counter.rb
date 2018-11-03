0b8ee5aae915770be1b72e04aa5dc4f94cafdeee
Chris Lu <chris.lu94@gmail.com>
2018-11-03 18:11:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 # Write your code here.
 def line(queue) 
   if queue.length > 0
+    print
     counter = 1
     array.each {
-      |x| 
-      
+      |name| 
+      print "The line is currently"
       counter += 1
     }
 end
